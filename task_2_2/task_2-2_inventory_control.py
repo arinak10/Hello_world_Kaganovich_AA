cdbd0f62bf5b2629ad7f2045947a61593916a807
arinak10 <arinakaganovic015@gmail.com>
2026-02-26 17:28:33 +0300

    Update task_2-2_inventory_control.py

--- task_2_2/task_2-2_inventory_control.py
+++ task_2_2/task_2-2_inventory_control.py
@@ -1,3 +1,4 @@
-reagent = "хлорид натрия"
-amount = "100"
-print(f"Реактив {reagent} поступил на склад в количестве {amount} шт.")
+reagent = input("Введите название реактива: ")
+amount = int(input("Введите количество: "))
+with open("inventory.txt", "w", encoding="utf-8") as inventory:
+ inventory.write (f"Реактив {reagent} поступил на склад в количестве {amount} шт." )
